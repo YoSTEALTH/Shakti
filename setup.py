5db43dc65ec58acb62b16cb18fde75410bbb780e
YoSTEALTH <ritesh@stealthcentral.com>
2024-03-28 21:11:00 -0600

    setup: using `/tmp/liburing-source` as C library header source.

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from os import cpu_count
+from os.path import join
+from tempfile import gettempdir
 from setuptools import setup
 from Cython.Build import cythonize
 from Cython.Compiler import Options
@@ -7,12 +9,13 @@ from Cython.Distutils import Extension
 
 debug = False  # <- manually change this
 threads = cpu_count()//2 or 1  # use half of cpu resources
+liburing_source = join(gettempdir(), 'liburing-source')  # /tmp/liburing-source
 
 # compiler options
 Options.docstrings = True
 if debug:
     Options.warning_errors = True  # turn all warnings into errors.
-    Options.fast_fail = True
+    Options.fast_fail = False
     Options.annotate = True  # generate `*.html` file for debugging & optimization.
 else:
     Options.warning_errors = False
@@ -21,6 +24,9 @@ else:
 extension = [Extension(name='shakti.*',  # where the `.so` will be saved.
                        sources=['src/shakti/*/*.pyx'],
                        language='c',
+                       libraries=['uring'],
+                       library_dirs=[join(liburing_source, 'src')],
+                       include_dirs=[join(liburing_source, 'src/include')],
                        extra_compile_args=['-O3', '-g0'],  # optimize & remove debug symbols+data
                        define_macros=[('CYTHON_TRACE_NOGIL', 1 if debug else 0)])]
 
