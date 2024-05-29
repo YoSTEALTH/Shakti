5c7654f34728d68b58984f610cb20d399d9d5328
YoSTEALTH <ritesh@stealthcentral.com>
2024-05-29 13:35:43 -0600

    random: improved function & test

--- src/shakti/os/random.pyx
+++ src/shakti/os/random.pyx
@@ -1,27 +1,25 @@
 from liburing.common cimport iovec, io_uring_prep_close
-from liburing.file cimport io_uring_prep_openat, io_uring_prep_readv
+from liburing.file cimport io_uring_prep_openat, io_uring_prep_read
 from ..event.entry cimport SQE
 
 
-async def random(size_t length)-> bytes:
+async def random(unsigned int length)-> bytes:
     ''' Async Random
 
         Example
             >>> await random(3)
             b'4\x98\xde'
     '''
+    if length == 0:  return b''
     cdef:
-        int fd
-        bytes buffer = bytes(length)
-        iovec iov = iovec(buffer)
-        SQE sqe = SQE(), sqes = SQE(2)
-
-    io_uring_prep_openat(sqe, b'/dev/random')   # open
+        SQE             sqe = SQE(), sqes = SQE(2)
+        bytearray       buffer = bytearray(length)
+        unsigned int    result
+    # open
+    io_uring_prep_openat(sqe, b'/dev/random')
     await sqe
-    fd = sqe.result
-
-    io_uring_prep_readv(sqes[0], fd, iov)       # read &
-    io_uring_prep_close(sqes[1], fd)            # close
+    # read & close
+    io_uring_prep_read(sqes, sqe.result, buffer, length)
+    io_uring_prep_close(sqes[1], sqe.result)
     await sqes
-    
-    return buffer
+    return bytes(buffer if sqes.result == length else buffer[:sqes.result])
--- test/os/random_test.py
+++ test/os/random_test.py
@@ -6,5 +6,10 @@ def test_random():
 
 
 async def main():
+    data = await shakti.random(0)
+    assert len(data) == 0
+    assert data == b''
+
     data = await shakti.random(12)
     assert len(data) == 12
+    assert data != b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
