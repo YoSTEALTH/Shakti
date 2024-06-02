36fd0f787d33349dac8ef0cae8d9816f4b4d957e
YoSTEALTH <ritesh@stealthcentral.com>
2024-06-01 23:45:51 -0600

    test: improved `test_SQE`

--- test/event/sqe_test.py
+++ test/event/sqe_test.py
@@ -5,43 +5,45 @@ import shakti
 
 
 def test_SQE():
-    print()
     shakti.run(
-        zero_sqe(),
-        zero_sqes(2),
-        # zero_sqes(1024),  # TODO: segfault, look into it.
-        with_statement(),
+        single_sqe(),
+        multiple_sqes(1000),
+        with_statement()
     )
 
 
-async def zero_sqe():
+async def single_sqe():
     sqe = shakti.SQE()
     liburing.io_uring_prep_openat(sqe, b'/dev/zero')
     await sqe
-    assert sqe.result > 0
+    await shakti.close(fd := sqe.result)  # fd
+    assert fd > 0
 
     with pytest.raises(ValueError):
         shakti.SQE(1025)
 
 
-async def zero_sqes(loop):
-    sqes = shakti.SQE(loop)
+async def multiple_sqes(loop):
+    sqes = shakti.SQE(loop, True)
     for i in range(loop):
         liburing.io_uring_prep_openat(sqes[i], b'/dev/zero')
     await sqes
     for i in range(loop):
-        assert sqes[i].result > 0
+        await shakti.close(fd := sqes[i].result)
+        assert fd > 0
 
 
 async def with_statement():
     # single
     async with shakti.SQE(error=False) as sqe:
         liburing.io_uring_prep_openat(sqe, b'/dev/zero')
-    assert sqe.result > 0
+    await shakti.close(fd := sqe.result)  # fd
+    assert fd > 0
 
     #  multiple
     async with shakti.SQE(2, error=False) as sqe:
         liburing.io_uring_prep_openat(sqe, b'/bad-link')
         liburing.io_uring_prep_openat(sqe[1], b'/dev/zero')
     assert sqe.result == -errno.ENOENT
-    assert sqe[1].result > 0
+    await shakti.close(fd := sqe[1].result)  # fd
+    assert fd > 0
