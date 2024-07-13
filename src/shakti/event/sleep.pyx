02fe2bf7eee3dcfc956e599508af425506a0a0dc
YoSTEALTH <ritesh@stealthcentral.com>
2024-07-13 03:14:51 -0600

    sleep: refactored `sleep`

--- src/shakti/event/sleep.pyx
+++ src/shakti/event/sleep.pyx
@@ -1,12 +1,10 @@
 from libc.errno cimport ETIME
-from liburing.lib.type cimport __u64, uint8_t
 from liburing.error cimport trap_error
-from liburing.queue cimport IOSQE_ASYNC, io_uring_sqe_set_flags, io_uring_sqe_set_data64
 from liburing.time cimport timespec, io_uring_prep_timeout
-from .entry cimport ENTRY, SQE
+from .entry cimport SQE
 
 
-async def sleep(double second, uint8_t flags=0)-> None:
+async def sleep(double second, unsigned int flags=0):
     '''
         Type
             second: int | float     # double
@@ -27,11 +25,7 @@ async def sleep(double second, uint8_t flags=0)-> None:
     cdef:
         timespec    ts = timespec(second)  # prepare timeout
         SQE         sqe = SQE(1, False)
-
-    sqe.job = ENTRY
     io_uring_prep_timeout(sqe, ts, 0, flags)  # note: `count=1` means no timer!
-    io_uring_sqe_set_flags(sqe, IOSQE_ASYNC)
-    io_uring_sqe_set_data64(sqe, <__u64><void*>sqe)  # just get `sqe` address
     await sqe
     # note: `ETIME` is returned as result for successfully timing-out.
     if sqe.result != -ETIME:
