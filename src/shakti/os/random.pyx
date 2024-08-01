3619eee3210831df90efe984cb06f732063850b8
YoSTEALTH <ritesh@stealthcentral.com>
2024-08-01 13:13:28 -0600

    os: renamed `random` to `random_bytes` as that's what the function does
    returns random bytes.

--- src/shakti/os/random.pyx
+++ src/shakti/os/random.pyx
@@ -3,24 +3,30 @@ from liburing.file cimport io_uring_prep_openat, io_uring_prep_read
 from ..event.entry cimport SQE
 
 
-async def random(unsigned int length)-> bytes:
-    ''' Async Random
+async def random_bytes(unsigned int length)-> bytes:
+    ''' Async Random Bytes
 
         Example
-            >>> await random(3)
+            >>> await random_bytes(3)
             b'4\x98\xde'
     '''
-    if length == 0:  return b''
+    if length == 0:
+        return b''
+
     cdef:
         SQE             sqe = SQE(), sqes = SQE(2)
         bytearray       buffer = bytearray(length)
         unsigned int    result
+
     # open
-    io_uring_prep_openat(sqe, b'/dev/random')
+    io_uring_prep_openat(sqe, b'/dev/urandom')
     await sqe
+    result = sqe.result  # fd
+
     # read & close
-    io_uring_prep_read(sqes, sqe.result, buffer, length)
-    io_uring_prep_close(sqes[1], sqe.result)
+    io_uring_prep_read(sqes, result, buffer, length)
+    io_uring_prep_close(sqes[1], result)
     await sqes
-    result = sqes.result  # warning: int == unsigned int
+    result = sqes.result
+
     return bytes(buffer if result == length else buffer[:result])
