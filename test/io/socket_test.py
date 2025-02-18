a117dc862a673a0ea24a41d981eeb02ae21ba1ca
Ritesh <ritesh@stealthcentral.com>
2025-02-18 00:03:04 -0600

    test.socket: GitHub doesn't support Linux 6.11+ yet! so skip socket test

--- test/io/socket_test.py
+++ test/io/socket_test.py
@@ -2,6 +2,7 @@ import pytest
 import shakti
 
 
+@pytest.mark.skip_linux(6.11)
 def test_socket():
     random_port = []
     shakti.run(
@@ -34,7 +35,6 @@ async def socket():
     # await shakti.close(sock_fd, True)
 
 
-@pytest.mark.skip_linux(6.11)
 async def bind():
     # IPv4
     sockfd = await shakti.socket()
@@ -54,7 +54,6 @@ async def bind():
     # await shakti.close(sockfd)
 
 
-@pytest.mark.skip_linux(6.11)
 async def listen():
     sockfd = await shakti.socket()
     await shakti.bind(sockfd, '127.0.0.1', 0)
@@ -62,7 +61,6 @@ async def listen():
     await shakti.close(sockfd)
 
 
-@pytest.mark.skip_linux(6.11)
 async def echo_server(random_port):
     assert (server_fd := await shakti.socket()) > 0
     try:
